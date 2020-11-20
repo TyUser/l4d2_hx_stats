384d6822135fcece32dead7a26913ff6a20a7fb9
TyUser <enter20@mail.ru>
2020-11-20 18:57:51 +0500

    Подправлено форматирование
    Более удобное для восприятия форматирование.

--- l4d2_stats_web/system/hx_config.php
+++ l4d2_stats_web/system/hx_config.php
@@ -1,13 +1,15 @@
 <?php
-if(!defined('HX_STATS')){exit();}
+if (!defined('HX_STATS')) {
+    exit();
+}
 
-$aDB=array();
+$aDB = array();
 $aDB['server'] = 'localhost';
 $aDB['username'] = 'your_user';
 $aDB['passwd'] = 'your_pass';
 $aDB['dbname'] = 'your_db';
 
-$aConf=array();
+$aConf = array();
 $aConf['title'] = 'Stats players:';
 $aConf['description'] = '[L4D2] hx_stats (Co-op)';
 $aConf['keywords'] = '[L4D2] hx_stats (Co-op)';
